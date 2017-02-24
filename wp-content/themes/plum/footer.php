df239e80f509f0f9f4b2f84820021f491ce081e9
rolenweb <rolenweb@yandex.ru>
2017-02-24 09:12:13 +0300

    counter

--- wp-content/themes/plum/footer.php
+++ wp-content/themes/plum/footer.php
@@ -18,6 +18,18 @@
 			<span class="sep"></span>
 			<?php echo '&copy; '.date('Y').'. All Rights Reserved.'; ?>
 		</div><!-- .site-info -->
+		<div class="text-center">
+			<!--LiveInternet counter--><script type="text/javascript">
+document.write("<a href='//www.liveinternet.ru/click' "+
+"target=_blank><img src='//counter.yadro.ru/hit?t44.8;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet' "+
+"border='0' width='31' height='31'><\/a>")
+</script><!--/LiveInternet-->
+		</div>
 	</footer><!-- #colophon -->
 	
 </div><!-- #page -->
